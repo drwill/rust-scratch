48ffd21b709388c4a0dfde7e0ae1f0b9a8f4df42
David R. Williamson <drwill@microsoft.com>
2023-10-03 16:09:46 -0700

    Cleaner rust

--- k8s/k8s-crd/src/main.rs
+++ k8s/k8s-crd/src/main.rs
@@ -1,17 +1,20 @@
 #![deny(rust_2018_idioms)]
 #![warn(clippy::all, clippy::pedantic)]
 #![allow(
-    clippy::let_and_return,
+    clippy::default_trait_access,
     clippy::let_unit_value,
     clippy::missing_errors_doc,
-    clippy::too_many_arguments
+    clippy::must_use_candidate,
+    clippy::module_name_repetitions,
+    clippy::let_underscore_untyped,
+    let_underscore_drop
 )]
 
 pub mod events_enums;
 pub mod widget_spec;
 
 use events_enums::{EventType,EventReason};
-use widget_spec::{Widget, WidgetSpec};
+use widget_spec::Widget;
 
 use k8s_openapi::api::core::v1::{Event, ObjectReference};
 use k8s_openapi::apimachinery::pkg::apis::meta::v1::MicroTime;
@@ -27,51 +30,31 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     
     let client = kube::Client::try_default().await?;
     let widgets_client: kube::Api<Widget> = kube::Api::namespaced(client.clone(), namespace);
-    
-    let resource;
 
-    match widgets_client.get_opt(resource_name).await? {
-        Some(widget) => {
+    let resource = match widgets_client.get_opt(resource_name).await {
+        Ok(Some(widget)) => {
             debug!("Widget found {widget:?}");
-            resource = widget;
+            widget
         },
-        None => {
-            debug!("Widget not found; creating one.");
-            match widgets_client.create(
-                &PostParams::default(),
-                &Widget {
-                    metadata: ObjectMeta {
-                        name: Some(String::from(resource_name)),
-                        namespace: Some(String::from(namespace)),
-                        ..Default::default()
-                    },
-                    spec: WidgetSpec {
-                        age: Some(0),
-                    }
-                }
-            ).await {
-                Ok(widget) => {
-                    debug!("Created widget {widget:?}");
-                    resource = widget;
-                },
-                Err(err) => {
-                    panic!("Error creating widget due to {err}");
-                }
-            };
+        Ok(None) => {
+            panic!("Widget {resource_name} not found.");
+        },
+        Err(err) => {
+            panic!("Error getting widget {resource_name} due to {err:?}");
         }
-    }
+    };
 
-    let events_client: kube::Api<Event> = kube::Api::default_namespaced(client);
+    let events_client: kube::Api<Event> = kube::Api::namespaced(client, namespace);
 
     let utc_now = Utc::now();
     let my_event: Event = Event {
-        type_: Some(String::from(EventType::Normal.to_string())),
-        action: Some(String::from("Manipulated widget")),
+        type_: Some(EventType::Normal.to_string()),
+        action: Some("Manipulated widget".to_owned()),
         event_time: Some(MicroTime(utc_now)),
-        reason: Some(String::from(EventReason::Unhealthy.to_string())),
-        message: Some(String::from("Widget crumbled.")),
-        reporting_component: Some(String::from("drw-component")),
-        reporting_instance: Some(String::from("k8s-crd")),
+        reason: Some(EventReason::Unhealthy.to_string()),
+        message: Some("Widget crumbled.".to_owned()),
+        reporting_component: Some("drw-component".to_owned()),
+        reporting_instance: Some("k8s-crd".to_owned()),
         count: Some(1),
         first_timestamp: None,
         last_timestamp: None,
@@ -80,9 +63,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         related: None,
         // This is metadata about the object for which the event is raised.
         involved_object: ObjectReference {
-            api_version: Some(String::from("stable.example.com/v1")),
-            kind: Some(String::from("Widget")),
-            namespace: Some(String::from(namespace)),
+            api_version: Some("stable.example.com/v1".to_owned()),
+            kind: Some("Widget".to_owned()),
+            namespace: Some(namespace.to_owned()),
             name: resource.metadata.name.clone(),
             resource_version: resource.metadata.resource_version.clone(),
             uid: resource.metadata.uid.clone(),
@@ -92,20 +75,17 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         metadata: ObjectMeta {
             // This needs to be a unique name for the event, otherwise you'll get a 409 Conflict,
             // so we'll use the resource name and the number of nanoseconds since unix epoch time.
-            name: Some(format!("{:?}-{}", resource.metadata.name.clone().unwrap(), utc_now.timestamp_nanos())),
-            namespace: Some(String::from(namespace)),
+            name: Some(format!(
+                "{:?}-{}",
+                resource.metadata.name.unwrap_or(String::new()),
+                utc_now.timestamp_nanos())),
+            namespace: Some(namespace.to_owned()),
             ..Default::default()
         },
     };
 
-    match events_client.create(&PostParams::default(), &my_event).await {
-        Ok(event) => {
-            println!("event logged\n{event:#?}");
-        },
-        Err(err) =>{
-            panic!("error logging drw event: {err:?}");
-        },
-    };
+    let event =  events_client.create(&PostParams::default(), &my_event).await?;
+    println!("event logged\n{event:?}");
 
     Ok(())
 }
